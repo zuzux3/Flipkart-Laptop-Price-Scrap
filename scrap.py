7dd19643237a86d5e4d0ced3e9b79ac4e8ff991c
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2022-12-26 00:47:28 +0000

    scraper made

--- scrap.py
+++ scrap.py
@@ -8,3 +8,18 @@ products = []
 prices = []
 ratings = []
 driver.get('https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&amp;amp;amp;amp;amp;amp;amp;amp;amp;amp;uni')
+
+content = driver.page_source
+soup = BeautifulSoup(content)
+for a in soup.findAll('a', href=True, attrs={'class': '_1fQZEK'}):
+    name = a.find('div', attrs={'class':'_4rR01T'})
+    rating = a.find('div', attrs={'class':'_3LWZlK'})
+    price = a.find('div', attrs={'class', '_3tbKJL _1_WHN1'})
+    products.append(name.text)
+    ratings.append(rating.text)
+    prices.append(price.text)
+
+df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings}) 
+df.to_csv('products.csv', index=False, encoding='utf-8')
+
+print(df.head())
