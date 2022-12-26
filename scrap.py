61afb2b87c44c1739ec00b6cc69e0093196ec445
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2022-12-26 00:33:02 +0000

    product, prices, ratings declaration and use of .get() metod

--- scrap.py
+++ scrap.py
@@ -3,3 +3,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+products = []
+prices = []
+ratings = []
+driver.get('https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&amp;amp;amp;amp;amp;amp;amp;amp;amp;amp;uni')
